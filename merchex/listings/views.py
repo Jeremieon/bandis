1f174d3534ba54db0a7ebe032185607e2a00d772
Jeremieon <jerry_slade@outlook.com>
2022-11-03 22:18:12 +0100

    updated models

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About Us</h1> <p>We love merch</p>')
