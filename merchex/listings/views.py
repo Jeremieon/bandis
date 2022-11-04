cbbf5a96de166bafee38c029169a3f0652ae8a7a
Jeremieon <jerry_slade@outlook.com>
2022-11-04 22:29:12 +0100

    CRUD

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Band,listing
 from django.core.mail import send_mail
-from listings.forms import ContactUsForm
+from listings.forms import ContactUsForm,BandForm,ListForm
 # Create your views here.
 
 def band_list(request):
@@ -45,3 +45,73 @@ def contact(request):
         form = ContactUsForm()
     return render(request,'listings/contact.html',{'form':form})
     
+def band_create(request):
+    if request.method == 'POST':
+        form=BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail',band.id) #it will return to band detail page and show the current id
+    else:
+        form = BandForm()
+    return render(request,'listings/band_create.html',{'form': form})
+
+def listing_create(request):
+    if request.method == 'POST':
+        form=ListForm(request.POST)
+        if form.is_valid():
+            lista = form.save()
+            return redirect('listing_detail',lista.id) #it will return to band detail page and show the current id
+    else:
+        form = ListForm()
+    return render(request,'listings/listing_create.html',{'form': form})
+
+#update
+
+def band_update(request, id):
+    band = Band.objects.get(id=id)
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            # update the existing `Band` in the database
+            form.save()
+            # redirect to the detail page of the `Band` we just updated
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
+    form = BandForm(instance=band)
+    return render(request,'listings/band_update.html',{'form': form})
+    
+
+def listing_update(request, id):
+    listn = listing.objects.get(id=id)
+    if request.method == 'POST':
+        form = ListForm(request.POST, instance=listn)
+        if form.is_valid():
+            # update the existing `Band` in the database
+            form.save()
+            # redirect to the detail page of the `Band` we just updated
+            return redirect('listing_detail', listn.id)
+    else:
+        form = ListForm(instance=listn)
+    form = ListForm(instance=listn)
+    return render(request,'listings/list_update.html',{'form': form})
+
+
+def band_delete(request,id):
+    band = Band.objects.get(id=id)
+
+    if request.method == 'POST':
+        band.delete()
+
+        return redirect('band_list')
+    return render(request,'listings/band_delete.html',{'band':band})
+
+
+def listing_delete(request,id):
+    listn = listing.objects.get(id=id)
+
+    if request.method == 'POST':
+        listn.delete()
+
+        return redirect('listing_view')
+    return render(request,'listings/listing_delete.html',{'listn':listn})
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -10,5 +10,12 @@ urlpatterns = [
     path('listing/',views.listing_view,name='listing_view'),
     path('listing/<int:id>/', views.listing_detail, name='listing_detail'),
     path('contact-us/',views.contact,name='contact'),
+    path('bands/add/', views.band_create, name='band-create'),
+    path('listing/add/', views.listing_create, name='list-create'),
+    path('bands/<int:id>/change', views.band_update, name='band-update'),
+    path('listing/<int:id>/change', views.listing_update, name='listing-update'),
+    path('band/<int:id>/delete', views.band_delete, name='band-delete'),
+    path('listing/<int:id>/delete',views.listing_delete, name='listing-delete')
+
 
 ]
